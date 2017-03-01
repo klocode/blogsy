63764839712671cb483a46e4becfbc7a0d5b9d1b
klocode <kendrick.t.lo@gmail.com>
2017-02-28 22:00:21 -0500

    working on tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = create(:user)
+    @post = create(:post)
+    @post.user_id = @user.id
+  end
+
+  test "should get index" do
+    get '/posts'
+    assert_response :success
+    json = JSON.parse(response.body)
+    assert_equal hash[:title], json["title"]
+  end
+
 end
